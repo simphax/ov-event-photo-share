2fa1d53059ec40e9196da88ff84025999c5536e3
Simon Nilsson <sim.nilsson@gmail.com>
2024-08-02 22:09:34 +0200

    Fixed upload confirmation

--- frontend/src/components/ImagesUpload.tsx
+++ frontend/src/components/ImagesUpload.tsx
@@ -6,12 +6,15 @@ import { ImageItem } from "../types/ImageItem";
 import ImageGallery from "./ImageGallery";
 import "./ImagesUpload.css";
 import { getUserId } from "../services/UserService";
+import { SelectImages } from "./SelectImages";
 
 const delay = (ms: number): Promise<void> => {
   return new Promise((resolve) => setTimeout(resolve, ms));
 };
 
 const ImagesUpload: React.FC = () => {
+  const [oneUploadDone, setOneUploadDone] = useState<boolean>(false);
+
   const [pendingImageItems, setPendingImageItems] = useState<ImageItem[]>([]);
   const [uploadedImageItems, setUploadedImageItems] = useState<ImageItem[]>([]);
   const [downloadedImageItems, setDownloadedImageItems] = useState<ImageItem[]>(
@@ -143,6 +146,7 @@ const ImagesUpload: React.FC = () => {
             ...currentItems,
           ]);
         };
+        if(!oneUploadDone) setOneUploadDone(true);
       })
       .catch((error) => {
         console.error("Upload error for file", imageItem.name, error);
@@ -201,14 +205,6 @@ const ImagesUpload: React.FC = () => {
     ]);
   }, [uploadInProgress, pendingImageItems, uploadImages]);
 
-  // useEffect(() => {
-  //   setPendingImageAngles([
-  //     Math.floor(Math.random() * 61) - 30,
-  //     Math.floor(Math.random() * 61) - 30,
-  //     Math.floor(Math.random() * 61) - 30,
-  //   ]);
-  // }, [pendingImageItems.length]);
-
   let combinedProgress = 100;
 
   if (pendingImageItems.length) {
@@ -221,210 +217,20 @@ const ImagesUpload: React.FC = () => {
     combinedProgress = Math.round(100 * combinedProgress);
   }
 
-  let photoArea = (
-    <label className="bg-primary p-4 rounded-full text-primaryText relative overflow-hidden block text-center tracking-wider font-semibold">
-      <input
-        type="file"
-        multiple
-        accept="image/*"
-        onChange={selectImages}
-        className="cursor-pointer absolute inset-0 w-full h-full opacity-0"
-      />
-      Choose photos to share
-    </label>
-  );
-
-  const pendingtemsWithError = pendingImageItems.filter((item) => item.error);
-
-  if (uploadInProgress) {
-    photoArea = (
-      <div className="progress-container">
-        <div className="progress-images">
-          {pendingImageItems.slice(0, 3).map((imageItem, index) => (
-            <div
-              className="progress-images__image"
-              key={imageItem.id}
-              style={
-                {
-                  backgroundImage: `url(${imageItem.url})`,
-                  "--angle": `${pendingImageAngles[index]}deg`,
-                } as React.CSSProperties
-              }
-            />
-          ))}
-          <div className="progress-images__count">
-            <span className="text-sm tracking-wider font-semibold">
-              {pendingImageItems.length}
-            </span>
-          </div>
-        </div>
-
-        <div className="progress-progress">
-          <div className="progress-progress__text text-sm tracking-wider font-semibold mb-2">
-            Uploading...
-          </div>
-          <div className="progress">
-            <div
-              className="progress__bar"
-              role="progressbar"
-              aria-valuenow={combinedProgress}
-              aria-valuemin={0}
-              aria-valuemax={100}
-              style={{ width: combinedProgress + "%" }}
-            ></div>
-          </div>
-        </div>
-        <div className="progress-cancel">
-          <button
-            className="progress-cancel__button"
-            onClick={() => setPendingImageItems([])}
-          >
-            <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <line x1="18" y1="6" x2="6" y2="18"></line>
-                <line x1="6" y1="6" x2="18" y2="18"></line>
-              </svg>
-            </div>
-          </button>
-        </div>
-      </div>
-    );
-  } else if (pendingtemsWithError.length > 0) {
-    photoArea = (
-      <div className="progress-container">
-        <div className="progress-images">
-          {pendingImageItems.slice(0, 3).map((imageItem, index) => (
-            <div
-              className="progress-images__image"
-              key={imageItem.id}
-              style={
-                {
-                  backgroundImage: `url(${imageItem.url})`,
-                  "--angle": `${pendingImageAngles[index]}deg`,
-                } as React.CSSProperties
-              }
-            />
-          ))}
-          <div className="progress-images__count">
-            <span className="text-sm tracking-wider font-semibold">
-              {pendingImageItems.length}
-            </span>
-          </div>
-        </div>
-
-        <div className="progress-progress">
-          <div className="progress-progress__text text-sm tracking-wider font-semibold mb-2">
-            Could not upload all pictures
-          </div>
-          <button
-            className="progress-progress__button"
-            onClick={() => uploadImages()}
-          >
-            Try again
-          </button>
-        </div>
-        <div className="progress-cancel">
-          <button
-            className="progress-cancel__button"
-            onClick={() => setPendingImageItems([])}
-          >
-            <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <line x1="18" y1="6" x2="6" y2="18"></line>
-                <line x1="6" y1="6" x2="18" y2="18"></line>
-              </svg>
-            </div>
-          </button>
-        </div>
-      </div>
-    );
-  }
-
   return (
     <div>
-      {/* <div className="progress-container">
-        <div className="progress-images">
-          {downloadedImageItems.slice(0, 3).map((imageItem, index) => (
-            <div
-              className="progress-images__image"
-              key={imageItem.id}
-              style={
-                {
-                  backgroundImage: `url(${imageItem.url})`,
-                  "--angle": `${pendingImageAngles[index]}deg`,
-                } as React.CSSProperties
-              }
-            />
-          ))}
-          <div className="progress-images__count">
-            {downloadedImageItems.length > 3 && (
-              <span className="text-sm tracking-wider font-semibold">
-                {downloadedImageItems.length - 3}
-              </span>
-            )}
-          </div>
-        </div>
-
-        <div className="progress-progress">
-          <div className="progress-progress__text text-sm tracking-wider font-semibold mb-2">
-            Uploading...
-          </div>
-          <div className="progress">
-            <div
-              className="progress__bar"
-              role="progressbar"
-              aria-valuenow={combinedProgress}
-              aria-valuemin={0}
-              aria-valuemax={100}
-              style={{ width: 40 + "%" }}
-            ></div>
-          </div>
-        </div>
-        <div className="progress-cancel">
-          <button
-            className="progress-cancel__button"
-            onClick={() => setPendingImageItems([])}
-          >
-            <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <line x1="18" y1="6" x2="6" y2="18"></line>
-                <line x1="6" y1="6" x2="18" y2="18"></line>
-              </svg>
-            </div>
-          </button>
-        </div>
-      </div> */}
-      <div style={{ height: "100px" }}>{photoArea}</div>
+      <div style={{ height: "170px" }}>
+        <SelectImages
+          pendingImageItems={pendingImageItems}
+          setPendingImageItems={setPendingImageItems}
+          uploadImages={uploadImages}
+          uploadInProgress={uploadInProgress}
+          oneUploadDone={oneUploadDone}
+          pendingImageAngles={pendingImageAngles}
+          combinedProgress={combinedProgress}
+          selectImages={selectImages}
+        />
+      </div>
       <ImageGallery
         onDeleteImage={deleteImage}
         uploadedImageItems={sortedUploadedImageItems}
