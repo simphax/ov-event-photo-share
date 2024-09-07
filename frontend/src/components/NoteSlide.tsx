2ea91011799d3a77a3bee01cbaca0143dd67345c
Simon Nilsson <simon.nilsson@im.se>
2024-09-07 21:01:57 +0200

    Version 4.0.0

--- frontend/src/components/NoteSlide.tsx
+++ frontend/src/components/NoteSlide.tsx
@@ -30,7 +30,7 @@ export function NoteSlide({ slide }: { slide: SlideNote }) {
   return (
     <div>
       <div className="container pointer-events-none">
-        <div className="bg-primary rounded-xl font-serif text-primaryText px-8 py-12 min-w-64 leading-loose max-h-[70vh] overflow-scroll">
+        <div className="bg-primary rounded-3xl font-serif text-primaryText px-8 py-12 min-w-64 leading-loose max-h-[70vh] overflow-scroll">
           <pre className="font-serif break-normal whitespace-pre-wrap">
             {slide.note}
           </pre>
